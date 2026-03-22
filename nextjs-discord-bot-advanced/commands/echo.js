0914da2e73cddd9cf8dc4f131cddf067c39a9cf0
eddush <eddyshermant@gmail.com>
2026-03-22 11:02:47 +0200

    Delete nextjs-discord-bot-advanced directory

--- nextjs-discord-bot-advanced/commands/echo.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "echo",
-  execute(message, args) {
-    message.reply(args.join(" "));
-  },
-};
--- nextjs-discord-bot-advanced/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:20}}>
-      <h1>Advanced Next.js Discord Bot</h1>
-      <p>Open /api/bot to start the bot</p>
-    </div>
-  );
-}
